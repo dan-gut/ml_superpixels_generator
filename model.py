88655da90f428c452032299d5cbf424984fbd7bd
dan-gut <32713485+dan-gut@users.noreply.github.com>
2024-04-18 12:44:55 +0200

    Upload initial version of representation network

---
+++ model.py
@@ -0,0 +1,134 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+from torchsummary import summary
+
+
+class DoubleConv(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(DoubleConv, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_ch, out_ch, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_ch, out_ch, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True),
+        )
+
+    def forward(self, x):
+        x = self.conv(x)
+        return x
+
+
+class Up(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(Up, self).__init__()
+        self.up_scale = nn.ConvTranspose2d(in_ch, out_ch, kernel_size=2, stride=2)
+
+    def forward(self, x1, x2):
+        x2 = self.up_scale(x2)
+
+        diffY = x1.size()[2] - x2.size()[2]
+        diffX = x1.size()[3] - x2.size()[3]
+
+        x2 = F.pad(x2, [diffX // 2, diffX - diffX // 2, diffY // 2, diffY - diffY // 2])
+        x = torch.cat([x2, x1], dim=1)
+        return x
+
+
+class DownLayer(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(DownLayer, self).__init__()
+        self.pool = nn.MaxPool2d(2, stride=2, padding=0)
+        self.conv = DoubleConv(in_ch, out_ch)
+
+    def forward(self, x):
+        x = self.conv(self.pool(x))
+        return x
+
+
+class UpLayer(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(UpLayer, self).__init__()
+        self.up = Up(in_ch, out_ch)
+        self.conv = DoubleConv(in_ch, out_ch)
+
+    def forward(self, x1, x2):
+        a = self.up(x1, x2)
+        x = self.conv(a)
+        return x
+
+
+class UNet(nn.Module):
+    def __init__(self, out_dimensions=2):
+        super(UNet, self).__init__()
+        self.conv1 = DoubleConv(1, 64)
+        self.down1 = DownLayer(64, 128)
+        self.down2 = DownLayer(128, 256)
+        self.down3 = DownLayer(256, 512)
+        self.down4 = DownLayer(512, 1024)
+        self.up1 = UpLayer(1024, 512)
+        self.up2 = UpLayer(512, 256)
+        self.up3 = UpLayer(256, 128)
+        self.up4 = UpLayer(128, 64)
+        self.last_conv = nn.Conv2d(64, out_dimensions, kernel_size=1)
+
+    def forward(self, x):
+        x1 = self.conv1(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x1_up = self.up1(x4, x5)
+        x2_up = self.up2(x3, x1_up)
+        x3_up = self.up3(x2, x2_up)
+        x4_up = self.up4(x1, x3_up)
+        output = self.last_conv(x4_up)
+        return output
+
+
+class RepresentationUNet(nn.Module):
+    def __init__(self, unet_out_dimensions, patch_size, representation_len=2048):
+        super(RepresentationUNet, self).__init__()
+        self.unet = UNet(unet_out_dimensions)
+        self.softmax = nn.Softmax()
+        self.av_pool = nn.AvgPool2d(2, stride=2)
+        self.flat = nn.Flatten()
+        x_size, y_size = patch_size
+        fc_input_size = int(x_size // 2 * y_size // 2)
+        self.fc1 = nn.Linear(fc_input_size, representation_len // 2)
+        self.fc2 = nn.Linear(representation_len // 2, representation_len)
+
+        self._patch_coords = None
+
+    def forward(self, x):
+        x1 = self.unet(x)
+        x2 = self.softmax(x1)
+        x3 = self._crop_patch(x2)
+        x4 = self.av_pool(x3)
+        x5 = torch.sum(x4, dim=1, keepdim=True)
+        x6 = self.flat(x5)
+        x7 = self.fc1(x6)
+        output = self.fc2(x7)
+        return output
+
+    def set_patch_coordinates(self, patch_coordinates):
+        self._patch_coords = patch_coordinates
+
+    def _crop_patch(self, input_tensor):
+        if self._patch_coords is None:
+            raise RuntimeError("Patch coordinates has not been set!")
+
+        output_tensor = input_tensor[:, :, self._patch_coords[0][0]:self._patch_coords[0][1],
+                                     self._patch_coords[1][0]:self._patch_coords[1][1]]
+
+        self._patch_coords = None
+
+        return output_tensor
+
+
+if __name__ == "__main__":
+    rep = RepresentationUNet(100, (40, 40))
+    rep.set_patch_coordinates([[0, 40], [0, 40]])
+    summary(rep.cuda(), (1, 400, 400))
---
+++ train.py
@@ -0,0 +1,199 @@
+import os
+from pathlib import Path
+import random
+
+import matplotlib.pyplot as plt
+import nibabel as nib
+import numpy as np
+from PIL import Image
+import torch
+import torch.optim as optim
+from torch import nn
+from torchvision import transforms
+
+from model import RepresentationUNet
+
+random.seed(42)
+
+dino_resnet50 = torch.hub.load('facebookresearch/dino:main', 'dino_resnet50')
+
+model_folder = Path("model")
+model_folder.mkdir(exist_ok=True)
+model_path = "model/rep_net.pt"
+data_base_dir = os.path.join("Data", "images")
+
+test_img_ratio = 0.2
+
+# Model params
+patch_size = (40, 40)
+unet_output_classes = 200
+
+# Training params
+learning_rate = 0.001
+weight_decay = 1e-4
+momentum = 0.9
+checkpoint_interval = 10
+images_per_epoch = 50
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+img_transform = transforms.Compose([
+    transforms.PILToTensor(),
+    transforms.ConvertImageDtype(torch.float)])
+
+
+class RepresentationLoss(nn.Module):
+    def __init__(self):
+        super(RepresentationLoss, self).__init__()
+
+    def forward(self, pred_rep, target_rep):
+        return torch.linalg.norm(pred_rep-target_rep)
+
+    @staticmethod
+    def get_dino_rep(input_image, patch_coords):
+        input_image = input_image.repeat(3, 1, 1)  # RGB channels are required
+        patch = input_image[:, patch_coords[0][0]:patch_coords[0][1], patch_coords[1][0]:patch_coords[1][1]]
+        patch = patch.unsqueeze(0)
+
+        rep_model = dino_resnet50.to(device)
+        rep_model.eval()
+
+        patch = patch.to(device)
+        with torch.no_grad():
+            rep = rep_model(patch)
+
+        return rep.detach().cpu()
+
+
+def dataset_loader(test_set=False):
+    files_list = list(os.listdir(data_base_dir))
+    files_list = sorted(files_list)
+    train_list = files_list[:int((1 - test_img_ratio) * len(files_list))]
+    test_list = files_list[int((1 - test_img_ratio) * len(files_list)):]
+
+    while True:
+        if test_set:
+            chosen_file = random.choice(test_list)
+        else:
+            chosen_file = random.choice(train_list)
+
+        file_path = os.path.join(data_base_dir, chosen_file)
+        image = nib.load(file_path).get_fdata()
+
+        x_min = random.randint(0, image.shape[0] - patch_size[0])
+        x_max = x_min + patch_size[0]
+
+        y_min = random.randint(0, image.shape[1] - patch_size[1])
+        y_max = y_min + patch_size[1]
+
+        patch_coords = ((x_min, x_max), (y_min, y_max))
+
+        image = np.squeeze(image, axis=2)
+        image = Image.fromarray(image.astype(np.uint8))
+        image = img_transform(image)
+
+        yield image, patch_coords
+
+
+def train(epoch_number):
+    model = RepresentationUNet(unet_out_dimensions=unet_output_classes, patch_size=patch_size, representation_len=2048)
+    optimizer = optim.RMSprop(model.parameters(), lr=learning_rate, weight_decay=weight_decay, momentum=momentum)
+    model.to(device)
+
+    if os.path.isfile(model_path):
+        print("Loading model")
+        checkpoint = torch.load(model_path, map_location=torch.device(device))
+        model.load_state_dict(checkpoint['model_state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        curr_epoch = checkpoint['epoch'] + 1
+        losses = checkpoint['losses']
+    else:
+        curr_epoch = 0
+        losses = []
+
+    loss_fun = RepresentationLoss()
+    train_loader = dataset_loader(test_set=False)
+
+    for epoch in range(curr_epoch, curr_epoch + epoch_number):
+        print(f"Epoch {epoch}")
+        epoch_losses = []
+        for _ in range(images_per_epoch):
+            image, patch_coords = next(train_loader)
+            target = RepresentationLoss.get_dino_rep(image, patch_coords)
+
+            image = image.to(device)
+            target = target.to(device)
+
+            optimizer.zero_grad()
+            model.set_patch_coordinates(patch_coords)
+
+            output = model(image.unsqueeze(0))
+            loss = loss_fun(output, target)
+
+            loss.backward()
+            optimizer.step()
+            epoch_losses.append(loss.item())
+
+        mean_loss = np.mean(epoch_losses)
+        losses.append(mean_loss)
+        print(mean_loss)
+        if (epoch + 1) % checkpoint_interval == 0:
+            print("Saving model checkpoint")
+            torch.save({'epoch': epoch,
+                        'losses': losses,
+                        'model_state_dict': model.state_dict(),
+                        'optimizer_state_dict': optimizer.state_dict()
+                        }, model_path)
+
+    print("Saving model")
+    torch.save({'epoch': epoch,
+                'losses': losses,
+                'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict()
+                }, model_path)
+
+    return losses
+
+
+def test():
+    test_loader = dataset_loader(test_set=True)
+
+    model = RepresentationUNet(unet_out_dimensions=unet_output_classes, patch_size=patch_size, representation_len=2048)
+    model.to(device)
+
+    if os.path.isfile(model_path):
+        print("Loading model")
+        checkpoint = torch.load(model_path, map_location=torch.device(device))
+        model.load_state_dict(checkpoint['model_state_dict'])
+
+    loss_fun = RepresentationLoss()
+    losses = []
+
+    for _ in range(images_per_epoch):
+        image, patch_coords = next(test_loader)
+        target = RepresentationLoss.get_dino_rep(image, patch_coords)
+
+        image = image.to(device)
+        target = target.to(device)
+
+        model.set_patch_coordinates(patch_coords)
+        model.eval()
+
+        output = model(image.unsqueeze(0))
+
+        loss = loss_fun(output, target)
+        losses.append(loss.item())
+
+    mean_loss = np.mean(losses)
+    print(f"Test loss: {mean_loss}")
+
+    return mean_loss
+
+
+if __name__ == "__main__":
+    train_losses = train(50)
+    print(test())
+
+    plt.plot(train_losses)
+    plt.grid()
+    plt.savefig("loss.png")
